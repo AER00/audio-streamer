ae26976b1dd527ab800a10a2bea2955da4a60fbc
AeRo <pawellaszczyk2003@gmail.com>
2023-11-11 16:17:01 +0100

    stream in capture thread

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use anyhow::anyhow;
 use byteorder::{ByteOrder, LittleEndian};
-use cfg_if::cfg_if;
 use clap::Parser;
-use kanal::{bounded, Receiver, Sender};
 use pipewire as pw;
 use pipewire::spa::format::{MediaSubtype, MediaType};
 use pipewire::stream::StreamRef;
@@ -12,23 +10,11 @@ use spa::param::format_utils;
 use spa::pod::Pod;
 use std::net::{Ipv4Addr, UdpSocket};
 use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
 
 const CHUNK_SIZE: usize = 1024;
 
 const DATA_SILENCE: u8 = 0;
 const DATA_SOUND: u8 = 1;
-
-cfg_if! {
-    if #[cfg(target_pointer_width = "64")] {
-        type Format = u64;
-        const POINTER_BYTES: usize = 8;
-    } else {
-        type Format = u32;
-        const POINTER_BYTES: usize = 4;
-    }
-}
 
 /// Stream PC audio
 #[derive(Parser, Debug)]
@@ -51,39 +37,7 @@ struct UserData {
     format: spa::param::audio::AudioInfoRaw,
 }
 
-fn stream_audio(receiver: Receiver<Format>, socket: &UdpSocket) -> anyhow::Result<()> {
-    let mut buf = vec![0u8; CHUNK_SIZE + 1];
-    let mut silence;
-    let mut x;
-
-    socket.set_write_timeout(Some(Duration::from_secs(1)))?;
-
-    while !receiver.is_terminated() {
-        silence = true;
-
-        for bin in buf[1..].chunks_exact_mut(POINTER_BYTES) {
-            x = receiver.recv()?;
-            if x != 0 {
-                silence = false;
-            }
-            bin.copy_from_slice(&x.to_le_bytes());
-        }
-
-        if silence {
-            buf[0] = DATA_SILENCE;
-            LittleEndian::write_u16(&mut buf[1..3], (CHUNK_SIZE + 1) as u16);
-            socket.send(&buf[..3])?;
-            continue;
-        }
-
-        buf[0] = DATA_SOUND;
-        socket.send(&buf)?;
-    }
-
-    Ok(())
-}
-
-fn capture(stream: &StreamRef, sender: &Sender<Format>) -> anyhow::Result<()> {
+fn capture(stream: &StreamRef, sender: &UdpSocket, buf: &mut Vec<u8>) -> anyhow::Result<()> {
     let mut buffer = stream.dequeue_buffer().ok_or(anyhow!("no buffers"))?;
 
     let datas = buffer.datas_mut();
@@ -96,13 +50,28 @@ fn capture(stream: &StreamRef, sender: &Sender<Format>) -> anyhow::Result<()> {
 
     let samples = data.data().ok_or(anyhow!("no data"))?;
 
-    // transfer data in pointer size to reduce latency
-    for bin in samples[..end].chunks_exact(POINTER_BYTES) {
-        sender.send(Format::from_le_bytes(bin.try_into().unwrap()))?;
-    }
+    // // transfer data in pointer size to reduce latency
 
-    for bin in samples[..end].iter_mut() {
-        *bin = 0;
+    for bin in samples[..end].chunks(CHUNK_SIZE) {
+        let mut silence = true;
+
+        for x in bin {
+            if *x != 0 {
+                silence = false;
+                break;
+            }
+        }
+
+        if silence {
+            buf[0] = DATA_SILENCE;
+            LittleEndian::write_u16(&mut buf[1..3], (CHUNK_SIZE + 1) as u16);
+            sender.send(&buf[..3])?;
+            continue;
+        }
+
+        buf[0] = DATA_SOUND;
+        buf[1..CHUNK_SIZE + 1].copy_from_slice(bin);
+        sender.send(&buf[..CHUNK_SIZE + 1])?;
     }
 
     Ok(())
@@ -173,18 +142,12 @@ pub fn main() -> anyhow::Result<()> {
     let stream = pw::stream::Stream::new(&core, "audio-capture", props)?;
 
     let socket = Arc::new(UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0))?);
+    socket.set_nonblocking(true)?;
     socket.connect(args.address)?;
 
     let socket2 = socket.clone();
 
-    let (sender, receiver) = bounded(16384 / POINTER_BYTES);
-
-    thread::spawn(move || {
-        if let Err(e) = stream_audio(receiver, &socket) {
-            eprintln!("{}", e);
-            std::process::exit(1);
-        }
-    });
+    let mut buf = vec![0u8; 1600];
 
     let _listener = stream
         .add_local_listener_with_user_data(data)
@@ -192,7 +155,7 @@ pub fn main() -> anyhow::Result<()> {
             handle_param_change(id, user_data, param, &socket2, args.buffer)
         })
         .process(move |stream, _| {
-            if let Err(e) = capture(stream, &sender) {
+            if let Err(e) = capture(stream, &socket, &mut buf) {
                 eprintln!("{}", e);
                 std::process::exit(1);
             }
@@ -216,8 +179,8 @@ pub fn main() -> anyhow::Result<()> {
         std::io::Cursor::new(Vec::new()),
         &spa::pod::Value::Object(obj),
     )?
-    .0
-    .into_inner();
+        .0
+        .into_inner();
 
     let mut params = [Pod::from_bytes(&values).ok_or(anyhow!("cannot create Pod from_bytes"))?];
 
